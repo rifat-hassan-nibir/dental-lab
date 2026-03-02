8acc66c738035b0de20eb8b8c64715adae4b665c
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-03-03 05:14:18 +0600

    refactor: change border radius of button component

--- components/common/ui/Button.tsx
+++ components/common/ui/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({
   return (
     <button
       className={cn(
-        "flex justify-center items-center bg-primary text-white px-5 py-2.5 md:px-6 md:py-3 text-sm md:text-base font-semibold rounded-lg transition-all duration-300 hover:opacity-90 hover:scale-102 shadow-md hover:shadow-lg hover:cursor-pointer",
+        "flex justify-center items-center bg-primary text-white px-5 py-2.5 md:px-6 md:py-3 text-sm md:text-base font-semibold rounded-xl transition-all duration-300 hover:opacity-90 hover:scale-102 shadow-md hover:shadow-lg hover:cursor-pointer",
         className,
       )}
       onClick={onClick}
